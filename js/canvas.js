ae73a3f9cd981cd48780d0923c7a6cd657a7e7c8
chapman-darlene <31857968+chapman-darlene@users.noreply.github.com>
2019-02-13 13:39:21 -0500

    02132019_1
    Signed-off-by: chapman-darlene <31857968+chapman-darlene@users.noreply.github.com>

--- js/canvas.js
+++ js/canvas.js
@@ -1,42 +1,68 @@
-window.addEventListener("load", function () {
+function selectShape() {
+    var selection = document.getElementById("select1").value;
+    var theCanvas = document.getElementById("canvas1");
+    var shape = theCanvas.getContext("2d");
+
+    switch (selection) {
+        case "square":
+            shape.strokeStyle = "black";
+            shape.lineWidth = "2";
+            shape.strokeRect(25, 25, 25, 25);
+            break;
+
+        case "circle":
+            shape.beginPath();
+            shape.arc(145, 75, 20, 0, 2 * Math.PI, true);
+            shape.fillStyle = "#3a6b96";
+            shape.strokeStyle = "black";
+            shape.fill();
+            shape.stroke();
+            break;
+
+        case "rectangle":
+            shape.rect(20, 20, 50, 25);
+            shape.strokeStyle = "black";
+            shape.lineWidth = "1.5";
+            shape.fillStyle = "#d88960"
+            shape.stroke();
+
+            break;
+    }
+}
+
+
+
+
+
+
+
+/* window.addEventListener("load", function () {
 
     var theCanvas = document.getElementById("canvas1");
 
     if (theCanvas && theCanvas.getContext) {
         var shape = theCanvas.getContext("2d");
-        if (shape) {
-            //draw rectangle shape
-            shape.strokeStyle = "blue";
-            shape.lineWidth = "2";
-            shape.strokeRect(25, 25, 25, 25);
-            //build a shape, fillstyle, stroke, size
-            //build animation, transition, transformation
+        var selection = document.getElementById("select1").value;
+        switch (selection) {
+            case square:
+                var square = document.getElementById('square').values;
+                shape.strokeStyle = "black";
+                shape.lineWidth = "2";
+                shape.strokeRect(25, 25, 25, 25);
+                break;
+
+            case circle:
+                shape.beginPath();
+                shape.arc(75, 75, 100, 0, 2 * Math.PI, true);
+                shape.strokeStyle = "black";
+                shape.stroke();
+                break;
+
+            case rectangle:
+                shape.strokeStyle = "black";
+                shape.lineWidth = "2";
+                shape.strokeRect(25, 25, 100, 100);
+                break;
         }
     }
-
-})
-
-switch (shape) {
-    case square:
-        shape.strokeStyle = "blue";
-        shape.lineWidth = "2";
-        shape.strokeRect(25, 25, 25, 25);
-        break;
-
-    case circle:
-        break;
-
-    case rectangle:
-        shape.strokeStyle = "red";
-        shape.lineWidth = "2";
-        shape.strokeRect(25, 25, 100, 100);
-        break;
-
-    case triangle:
-        break;
-
-}
-
-function clearShape() {
-
-}
+}) */
--- js/css_elements.js
+++ js/css_elements.js
@@ -1,3 +1,42 @@
+/* function carMove() {
+    var car1 = document.getElementById("car1").style;
+    var car2 = document.getElementById("car2").style;
+    var car3 = document.getElementById("car3").style;
+    var car4 = document.getElementById("car4").style;
+    var car5 = document.getElementById("car5");
+    car1.id = "car-move";
+    car2.id = "car-move";
+    car3.id = "car-move";
+    car4.id = "car-move";
+    car5.id = "car-move";
+    resetCars();
+}
+
+function resetCars() {
+    var car1 = document.getElementById("car1")
+    var car2 = document.getElementById("car2");
+    var car3 = document.getElementById("car3");
+    var car4 = document.getElementById("car4");
+    var car5 = document.getElementById("car5");
+    car1.id = "car-start";
+    car2.id = "car-start";
+    car3.id = "car-start";
+    car4.id = "car-start";
+    car5.id = "car-start";
+} */
+
+function launch() {
+    var rocket1 = document.getElementById("rocket1");
+    var rocket2 = document.getElementById("rocket2");
+    var rocket3 = document.getElementById("rocket3");
+    var rocket4 = document.getElementById("rocket4");
+    rocket1.id = "lift";
+    rocket2.id = "lift";
+    rocket3.id = "lift";
+    rocket4.id = "lift";
+}
+
+
 function kick() {
     var kick = document.getElementById("kick1");
     var lift = document.getElementById("lift1");
