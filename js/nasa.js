2173f29c9f38272e8c58eb06c68ccae296becb56
chapman-darlene <31857968+chapman-darlene@users.noreply.github.com>
2019-02-23 19:31:43 -0500

    2232019_1
    Signed-off-by: chapman-darlene <31857968+chapman-darlene@users.noreply.github.com>

--- js/nasa.js
+++ js/nasa.js
@@ -13,9 +13,10 @@ var data = {};
 /* var d = new Date();
 var startDate = d.toISOString().slice(0, 10);
 var priorDate = d.setDate(d.toISOString().slice(0, 10) - 30);
- */
-console.log(startDate + priorDate);
-const arch_url = "https://api.nasa.gov/planetary/apod?api_key=" + apiKey + "&count=10";
+
+console.log(startDate + priorDate); */
+
+const arch_url = "https://api.nasa.gov/planetary/apod?api_key=" + apiKey + "&count=20";
 
 function arch_load() {
     const arch = new XMLHttpRequest();
@@ -23,43 +24,54 @@ function arch_load() {
     //onload of window load status and parse data
     arch.onreadystatechange = function () {
         if (this.readyState === 4 && this.status === 200) {
-            data = JSON.parse(this.response);
+            data = JSON.parse(this.responseText);
             console.log(data);
 
             //stringify data
             var listArray = new Array();
             for (var i = 0; i < data.length; i++) {
-                if (data[i].media_type == "image") {
-                    listArray.push(data[i].url);
-                }
-                localStorage.setItem("listArray", JSON.stringify(listArray));
-                //console.log(listArray);                
+                /* if (data[i].media_type == "image") { */
+                listArray.push(data[i]);
             }
+            localStorage.setItem("listArray", JSON.stringify(listArray));
+            //console.log(listArray);
 
-
-            var select = document.getElementById('dateSelect')
-
+            var select = document.getElementById("dateSelect")
             for (var j = 0; j < data.length; j++) {
-                select.innerHTML = select.innerHTML + '<option value ="' + '">' + data[j].date + '</option>';
-                //select.options[select.selectedIndex].value;
-                console.log(select.options[select.selectedIndex].value);
-                console.log(data[j].date);
+                //select.innerHTML = select.innerHTML + '<option value ="' + j + '">' + data[j].date + '</option>';
+                var obj = document.createElement("option");
+                obj.value = j;
+                obj.innerHTML = data[j].date;
+                select.appendChild(obj);
             }
+        } else {
+            document.getElementById('waiting').innerHTML = '<img src="img/solar.gif alt="rotating galaxy/>"';
         }
     };
     arch.send();
 }
-
 arch_load();
 
+function addInfo() {
+    var selection = document.getElementById("dateSelect").value;
+    data[selection];
+    document.getElementById("title").innerHTML = data[selection].title;
+    document.getElementById("url").src = data[selection].url;
+    document.getElementById("explanation").innerHTML = data[selection].explanation;
+}
 
+/* function slideshow() {
 
-/* 
-function slideshow() {
-    var imagePaths = [data.url];
+    var listArray = localStorage.getItem("listArray");
+    var imagePaths = JSON.parse(listArray);
+    //var images = document.getElementById("images");
+    for (var i = 0; i < imagePaths.length; i++) {
+        var image = document.createElement("img");
+        image.src = imagePaths[i].url;
+
+    }
     var showCanvas = null;
     var showCanvasCtx = null;
-    var img = document.createElement("img");
     var currentImage = 0;
     var revealTimer;
 
@@ -92,6 +104,6 @@ function slideshow() {
             clearInterval(revealTimer);
         showCanvasCtx.restore();
     }
-}
 
+}
 slideshow(); */
